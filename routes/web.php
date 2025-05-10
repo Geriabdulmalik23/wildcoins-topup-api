23b57369916afa7f5d74281bf8b3b2c7818df63b
Geri Abdul Malik <geriabdulmalik4@gmail.com>
2025-05-10 18:37:47 +0700

    Fix: use dynamic port from Railway environment

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->json('Welcome ');
 });
+Route::get('/test-jwt-secret', function () {
+    return env('JWT_SECRET', 'JWT_SECRET NOT FOUND');
+});
